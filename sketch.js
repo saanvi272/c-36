4a1f5af741da973564b5cbc313d951b173b9ac12
saanvi272 <73384497+saanvi272@users.noreply.github.com>
2020-12-25 01:37:03 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+var database,form,player,game;
+var gameState=0;
+var playerCount;
+
+function setup(){
+createCanvas(400,400);
+database=firebase.database();
+game=new Game();
+game.getState();
+game.start();
+
+
+}
+function draw(){
+  background("white");
+  
+
+
+}
+
+
